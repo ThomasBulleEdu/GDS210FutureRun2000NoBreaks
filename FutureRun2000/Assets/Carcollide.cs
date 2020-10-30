8039b38b404b1928b4b06e535bde79070b49969e
Josh Ayala <1020183@student.sae.edu.au>
2020-10-30 14:49:47 +1100

    Car Collider
    Car Collider code and prefab creation

---
+++ FutureRun2000/Assets/Carcollide.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carcollide : MonoBehaviour
+{
+    public GameObject speedo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider col)
+
+
+    {
+        if (col.gameObject.tag == "player")
+        {
+            speedo.GetComponent<speedTracking>().currentAccel = speedo.GetComponent<speedTracking>().currentAccel / 2;
+            Destroy(this.gameObject);
+        }
+     
+    
+     } 
+
+
+
+
+}
+    
+
+
+
